35f1104a4560c351c29dac1f156436f5c5955f26
Chris <thechrisyoon08@gmail.com>
2018-07-16 14:28:20 -0400

    touches

--- NeuralNetwork/test.py
+++ NeuralNetwork/test.py
@@ -7,22 +7,14 @@ if __name__ == "__main__":
     test.addLayer(1, "output", "sigmoid")
 
 
-    test.set_max_epoch(100)
+    test.set_max_epoch(50)
     test.set_desired_error(0.001)
     test.set_momentum(0.05)
     test.set_learning_rate(0.05)
-    test.set_erf("MSE") 
+    test.set_erf("MSE")
 
-    
+
     _input = [1, 1]
     output = [0]
 
     test.train(_input, output)
-    """
-    print("hidden layer bias")
-    print(test.layers[1].layer_type)
-    print(test.layers[1].bias)
-    print("biasbiasbiasbias")
-   """
-
-   
