b83c68947aa6bf78ea79c03f147487e4ff64567c
Chris <thechrisyoon08@gmail.com>
2018-07-16 13:26:25 -0400

    backprop matrix operation dimensions

--- NeuralNetwork/backpropagation.py
+++ NeuralNetwork/backpropagation.py
@@ -1,47 +1,36 @@
-from ANN import ANN
-from Layer import Layer
+from ANN import *
+from Layer import *
 import numpy as np
 
 def backpropagation(neuralnet):
 
-    #neuralnet = ann
-    #output layer
-    output_z = neuralnet.layers[len(neuralnet.layers) - 1].get_z_values()
-    output_dz = neuralnet.layers[len(neuralnet.layers) - 1].activation_function_derivative(output_z)
-    delta_output = neuralnet.loss_gradient() * output_dz
-    neuralnet.layers[len(neuralnet.layers) - 1].set_delta(delta_output)
+    #output layer delta
+    output_z = neuralnet.layers[len(neuralnet.layers) - 1].get_z_values() #column vector
+    output_dz = neuralnet.layers[len(neuralnet.layers) - 1].activation_function_derivative(output_z) #column vector
+    delta_output = neuralnet.loss_gradient() #* output_dz #column vector
+    neuralnet.layers[len(neuralnet.layers) - 1].set_delta(delta_output) #column vector
 
-
-    #hidden layer
+    #get hidden layer deltas
     for i in range(len(neuralnet.layers) - 2, 0, -1):
-        hidden_z = neuralnet.layers[i].get_z_values()
-        hidden_dz = neuralnet.layers[i].activation_function_derivative(hidden_z)
-        delta_hidden = np.transpose(np.matmul(np.transpose(neuralnet.layers[i].weight), np.transpose(neuralnet.layers[i+1].delta))) * hidden_dz
-        neuralnet.layers[i].set_delta(delta_hidden)
+        z_vals = neuralnet.layers[i].get_z_values()
+        a_val_derivatives = neuralnet.layers[i].activation_function_derivative(z_vals)
+        hidden_delta = np.matmul(np.transpose(neuralnet.layers[i].weight), neuralnet.layers[i+1].delta) * a_val_derivatives
+        neuralnet.layers[i].set_delta(hidden_delta)
 
-    #print(neuralnet.layers[1].delta)
-    #print(neuralnet.input)
+    #get delta weight
+    for i in range(0, len(neuralnet.layers) - 1, 1):
+        if i == 0:
+            a_vals = neuralnet._input
+        else:
+            a_vals = neuralnet.layers[i].get_activation_values()
 
-    #input layer
-    """print("dweight: " + str(neuralnet.layers[0].dWeight.shape))
-    print("delta: " + str(neuralnet.layers[1].delta.shape))
-    print("input: " + str(neuralnet.input.shape))"""
-    neuralnet.layers[0].dWeight = neuralnet.layers[0].dWeight + np.matmul(np.transpose(neuralnet.layers[1].delta), neuralnet.input)
-    """print("dBias: " + str(neuralnet.layers[0].dBias.shape))
-    print("delta: " + str(neuralnet.layers[1].delta.shape))
-    print("input: " + str(neuralnet.input.shape))"""
-    #neuralnet.layers[0].dBias = neuralnet.layers[0].dBias + np.matmul(np.transpose(neuralnet.layers[1].delta), neuralnet.input)
-    neuralnet.layers[0].dBias = neuralnet.layers[0].dBias + neuralnet.layers[1].delta
-    #print(len(neuralnet.layers))
-    #non_input layer dWeights
-    for i in range(1, len(neuralnet.layers) - 1, 1):
-        layer_a_values = neuralnet.layers[i].get_activation_values()
-        #print(neuralnet.layers[i+1].delta.shape)
-        #print(layer_a_values.shape)
-        #print(neuralnet.layers[i].dWeight.shape)
-        neuralnet.layers[i].dWeight = neuralnet.layers[i].dWeight+ np.matmul(neuralnet.layers[i+1].delta, layer_a_values)
-        #neuralnet.layers[i].dBias = neuralnet.layers[i].dBias + np.matmul(np.transpose(neuralnet.layers[i+1].delta), layer_a_values)
-        neuralnet.layers[i].dBias = neuralnet.layers[i].dBias + neuralnet.layers[i].delta
+        neuralnet.layers[i].dWeight = neuralnet.layers[i].dWeight + np.matmul(neuralnet.layers[i+1].delta, np.transpose(a_vals))
+        #neuralnet.layers[i].dBias = neuralnet.layers[i].dBias + np.matmul(neuralnet.layers[i+1].delta, np.transpose(a_vals))
+        neuralnet.layers[0].dBias = neuralnet.layers[0].dBias + neuralnet.layers[1].delta
+        print("+++++++++++")
+        print("bias")
+        print(neuralnet.layers[i].dBias)
+        print("+++++++++++")
 
-    print("backprop complete")
+
     return neuralnet
