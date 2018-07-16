3cf1fa2b69b28afc5ae1cfad012b063794fe0b5e
Chris <thechrisyoon08@gmail.com>
2018-07-16 13:26:42 -0400

    neural network test

---
+++ NeuralNetwork/test.py
@@ -0,0 +1,28 @@
+from ANN import *
+
+if __name__ == "__main__":
+    test = ANN()
+    test.addLayer(2, 3, "input")
+    test.addLayer(3, 1, "sigmoid")
+    test.addLayer(1, "output", "sigmoid")
+
+
+    test.set_max_epoch(100)
+    test.set_desired_error(0.001)
+    test.set_momentum(0.05)
+    test.set_learning_rate(0.05)
+    test.set_erf("MSE") 
+
+    
+    _input = [1, 1]
+    output = [0]
+
+    test.train(_input, output)
+    """
+    print("hidden layer bias")
+    print(test.layers[1].layer_type)
+    print(test.layers[1].bias)
+    print("biasbiasbiasbias")
+   """
+
+   
