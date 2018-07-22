0f45db59f439ef750705de81c9a42d757524f64c
Chris <thechrisyoon08@gmail.com>
2018-07-22 12:54:36 -0400

    adam

--- NeuralNetwork/test.py
+++ NeuralNetwork/test.py
@@ -1,19 +1,32 @@
 from ANN import *
+from adam import *
 
 if __name__ == "__main__":
     test = ANN()
-    test.createFromArrays([2,3,1],["sigmoid","sigmoid"])
+    test.createFromArrays([2,2,1],["linear","sigmoid"])
 
     test.set_max_epoch(50)
-    test.set_desired_error(0.001)
+    test.set_desired_error(0.00000001)
     test.set_momentum(0.05)
     test.set_learning_rate(0.05)
     test.set_erf("MSE")
 
 
+    #_input =[[1, 0], [0, 1]], [1,1], [0, 0]]
+    #output = [[1], [1]], [0], [0]]
+
+    #test.batch_train(_input, output, 1)
+
+    #test adam
+
     _input = [1, 1]
-    output = [0]
+    target = [0]
+    test.run(_input)
+    print(test.output)
 
-    test.train(_input, output)
+    test = ADAM(test)
+    test.adam(_input, target)
+    test = test.get_ann()
 
-
+    test.run(_input)
+    print(test.output)
